44e27f8ec3fe6030c103ee021318ac504e026ee3
Ali Sabir <m.alisabir00@gmail.com>
2026-08-11 17:11:31 +0500

    Fix Streamlit app

--- Day36/app.py
+++ Day36/app.py
@@ -16,8 +16,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "yolov8n.pt")
 DATA_YAML = os.path.join(BASE_DIR, "data.yaml")
 RESULTS_DIR = os.path.join(BASE_DIR, "results")
-IMAGE_DIR = os.path.join(BASE_DIR, "test100", "images")
-LABEL_DIR = os.path.join(BASE_DIR, "test100", "labels")
 os.makedirs(RESULTS_DIR, exist_ok=True)
 
 model = YOLO(MODEL_PATH)
