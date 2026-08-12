3daef4d712ea49a1931aecbe1153295af7509740
Ali Sabir <m.alisabir00@gmail.com>
2026-08-12 14:48:00 +0500

    Add Day37 YOLO Model Improvement

--- Day37/app.py
+++ Day37/app.py
@@ -34,10 +34,7 @@ v1, v2 = load_models()
 # V1 VS V2 METRICS
 # --------------------------------------------------
 
-st.markdown(
-    "<h2 style='text-align: center;'>V1 vs V2 Performance</h2>",
-    unsafe_allow_html=True
-)
+st.header("V1 vs V2 Performance")
 
 metrics = {
     "Precision": [0.3595, 0.8041],
